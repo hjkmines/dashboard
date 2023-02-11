8b0204605d17827718ce43ca1f580b8719ad755b
Chris Medyna <chris.medyna@gmail.com>
2023-02-11 14:52:29 -0500

    diff pages + routes

--- client/src/App.js
+++ client/src/App.js
@@ -12,12 +12,16 @@ import SharedLayout from "./layouts/SharedLayout";
 
 // Pages - all relevant pages for FE endpoints
 import HomePage from "./pages/HomePage";
+import AboutUs from "./pages/AboutUs";
+import Profile from "./pages/Profile";
 
 // modern syntax for browser router. Function to create Browser router. Nested function inside to create the routes from the elements themselves 
 const homeRouter = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<SharedLayout />} >
             <Route index element={<HomePage />} />
+            <Route  path='about' element={<AboutUs />} />
+            <Route  path='profile' element={<Profile />} />
             {/* add other routes here, all nested elemetns   */}
         </Route>
             
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Paper, Typography } from "@mui/material";
 
 
 const Navbar = () => {
   return (
     <Box>
       <Container>
+        <Paper elevation={2}>
           <Typography>
             This is the Navbar!
           </Typography>
+        </Paper>
       </Container>
     </Box>
   )
